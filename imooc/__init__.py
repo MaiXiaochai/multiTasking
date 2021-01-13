2e79ae9603546c587920dd0f1b50b8caba70df38
MaiXiaochai <maixiaochai@qq.com>
2021-01-13 22:53:04 +0800

    +)dir imooc

---
+++ imooc/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+
+"""
+------------------------------------------
+@File       : __init__.py
+@Author     : maixiaochai
+@Email      : maixiaochai@outlook.com
+@CreatedOn  : 2021/1/13 21:16
+------------------------------------------
+"""
